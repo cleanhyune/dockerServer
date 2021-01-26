f81de19d90e06a8f0207a588450f777d36d86633
johyune <sharpjo4@naver.com>
2021-01-26 10:32:03 +0900

    Initial commit

---
+++ src/main/java/com/nas/server/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.nas.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll();
+    }
+}
